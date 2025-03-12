4ffb2b15352217285a84e91deb014e87de12716e
amd <adcpetechsoft@hotmail.com>
2025-03-12 18:24:03 +0800

    Proto

---
+++ spelling_number.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+void spell(int numtospell)
+{
+
+    using namespace std;
+
+    int num;
+    int y;
+
+    num=numtospell;
+
+
+
+
+
+};
+
+
+int main(int argc, char *argv[])
+{
+    using namespace std;
+
+    if( argc > 1 )
+    {
+
+    }else
+    {
+        cout<<"ERROR: No ARG input."<<endl;
+    };
+
+
+    return 0;
+
+};
+
+
