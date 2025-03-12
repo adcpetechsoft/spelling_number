6f9dac58c8a61ed4f5de843bd86362005e1f9442
amd <adcpetechsoft@hotmail.com>
2025-03-12 19:19:16 +0800

    working01

--- spelling_number.cpp
+++ spelling_number.cpp
@@ -1,5 +1,52 @@
+#define STR_MAX_SIZE 50
+
 #include <iostream>
 #include <string>
+
+void spell_ones(int numtospell_ones)
+{
+    using namespace std;
+
+    if(numtospell_ones==1)
+    {
+        cout<<"One ";
+    };
+    if(numtospell_ones==2)
+    {
+        cout<<"Two ";
+    };
+    if(numtospell_ones==3)
+    {
+        cout<<"Three ";
+    };
+    if(numtospell_ones==4)
+    {
+        cout<<"Four ";
+    };
+    if(numtospell_ones==5)
+    {
+        cout<<"Five ";
+    };
+    if(numtospell_ones==6)
+    {
+        cout<<"Six ";
+    };
+    if(numtospell_ones==7)
+    {
+        cout<<"Seven ";
+    };
+    if(numtospell_ones==8)
+    {
+        cout<<"Eight ";
+    };
+    if(numtospell_ones==9)
+    {
+        cout<<"Nine ";
+    };
+    
+
+
+};
 
 void spell(int numtospell)
 {
@@ -10,6 +57,27 @@ void spell(int numtospell)
     int y;
 
     num=numtospell;
+
+    y=num/1000;
+    if(y>=1)
+    {
+        spell_ones(y);
+        cout<<"Thousand ";
+
+        num=num-(y*1000);
+    };
+
+    y=num/100;
+    if(y>=1)
+    {
+        spell_ones(y);
+        cout<<"Hundred ";
+
+        num=num-(y*100);
+
+    };
+
+    cout<<endl;
 
 
 
@@ -22,8 +90,25 @@ int main(int argc, char *argv[])
 {
     using namespace std;
 
+    int i;
+    int j;
+
+    string str01;
+
     if( argc > 1 )
     {
+        i=1;
+
+        while(i<argc)
+        {
+            str01=argv[i];
+            j=stoi(str01);
+            spell(j);
+            
+            i++;
+        };
+        
+
 
     }else
     {
